0dfcee501007fc230841ddfa972205918054d01a
vz-ille <emmanvellevz@gmail.com>
2025-03-31 22:09:11 -0400

    Configurando el puerto para Render

--- server.py
+++ server.py
@@ -1,36 +1,33 @@
-from flask import Flask, send_from_directory, jsonify, redirect
+from flask import Flask, jsonify
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Define la carpeta donde se almacenarán los archivos de actualización (por ejemplo, el APK)
-UPLOAD_FOLDER = 'updates'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Asegúrate de que la carpeta de carga exista
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-# URL pública donde has alojado tu archivo APK en MediaFire
-APK_PUBLIC_URL = 'https://download937.mediafire.com/sngpruhodv2gStOIX2W25bIS9z01nJ2UTEqX_9XQ3YxsOJT-HdOaLGiy6enVwTzvtLQBDcd4GeQIlVNp7lZNDBYkr3tkXSc8qEk2saEamcnXuKkIFk-yxLoXUfIxzSzKDxX5FUkvB_yMPVf4wvVegcEi8d7yf3fJzgHXLPw1zp_8zwg/xw0pegnmiwta7zl/app.apk'
-
-# Ruta para redirigir a la URL pública del APK
-@app.route('/download_apk')
-def download_apk():
-    return redirect(APK_PUBLIC_URL)
-
-# API endpoint para verificar si hay una nueva versión disponible
 @app.route('/api/check_update')
 def check_update():
-    # Aquí podrías tener lógica más compleja para comparar versiones
-    # Por ahora, simplemente indicamos que siempre hay una actualización disponible para simplificar
-    update_available = True
+    updates_folder = os.path.join(os.getcwd(), 'updates')
+    apk_path = os.path.join(updates_folder, 'app.apk')
+
+    if os.path.exists(apk_path):
+        update_available = True
+        download_url = '/updates/app.apk'  # La URL relativa para descargar el APK
+    else:
+        update_available = False
+        download_url = None
+
     return jsonify({
         'update_available': update_available,
-        'download_url': '/download_apk'
+        'version_code': 2,  # Puedes ajustar esto según tu lógica de versiones
+        'download_url': download_url
     })
 
+@app.route('/updates/<filename>')
+def updates(filename):
+    updates_folder = os.path.join(os.getcwd(), 'updates')
+    return send_from_directory(updates_folder, filename)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port, host='0.0.0.0')
